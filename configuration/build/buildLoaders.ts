835c3a8aad7d4aece9c95a716ea914df8b524919
Artem <varivonchikartem@gmail.com>
2024-12-08 07:50:19 -0800

    Add: Create global SCSS variables, integrate custom fonts, and configure fontLoader in Webpack

--- configuration/build/buildLoaders.ts
+++ configuration/build/buildLoaders.ts
@@ -1,18 +1,17 @@
 import { BuildOptions } from "./types/configuration";
-
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const { isDevelopment } = options;
 
-  const typescriptLoader = {
+  const typescriptLoader: webpack.RuleSetRule = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  const cssLoader = {
+  const cssLoader: webpack.RuleSetRule = {
     test: /\.s[ac]ss$/i,
     use: [
       isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
@@ -22,7 +21,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
           modules: {
             namedExport: false,
             exportLocalsConvention: "as-is",
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            auto: (resPath: string) => resPath.includes(".module."),
             localIdentName: isDevelopment ? "[path][name]__[local]--[hash:base64:5]" : "[hash:base64:8]",
           },
         },
@@ -31,15 +30,26 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  const svgrLoader = {
+  const svgLoader: webpack.RuleSetRule = {
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   };
 
-  const assetModulesLoader = {
-    test: /\.(png|jpe?g|ico)$/,
+  const fontLoader: webpack.RuleSetRule = {
+    test: /\.woff2$/i,
     type: "asset/resource",
+    generator: {
+      filename: "fonts/[name].[hash:8][ext]",
+    },
   };
 
-  return [typescriptLoader, cssLoader, svgrLoader, assetModulesLoader];
+  const imageLoader: webpack.RuleSetRule = {
+    test: /\.(png|jpe?g|gif|ico)$/i,
+    type: "asset/resource",
+    generator: {
+      filename: "images/[name].[hash:8][ext]",
+    },
+  };
+
+  return [typescriptLoader, cssLoader, svgLoader, fontLoader, imageLoader];
 }
